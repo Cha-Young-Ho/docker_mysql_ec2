eb76fa0571d5ac2d4e7afa130f1d54fa8ef6fabb
Cha-Young-Ho <jkijki1249@gmail.com>
2022-07-21 21:35:41 +0900

    Api-docs : 문서수정

---
+++ src/test/java/com/psc/j211/test.java
@@ -0,0 +1,17 @@
+package com.psc.j211;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Slf4j
+public class test {
+
+    @Test
+    @DisplayName("git action 테스트")
+    public void gitActionTest(){
+        log.info("테스트 실행되었음");
+    }
+
+}
